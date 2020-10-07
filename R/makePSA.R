d38ba72acc5d0182837645341e30f7b30a258f6a
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2020-10-07 19:55:27 +0100

    including PSA-maker

---
+++ R/makePSA.R
@@ -0,0 +1,32 @@
+##' This function facilitates generating a PSA data.table object from a parameter object
+##'
+##' @title Make PSA data.table from parameter object
+##' @param N number of samples (rows)
+##' @param P parameter object (eg from parse.parmtable)
+##' @param dbls list of 2-D name vectors for any parameter pairs generated jointly (same order as in \code{P})
+##' @return \code{data.table}
+##' @author Pete Dodd
+##' @import data.table
+##' @export
+makePSA <- function(N,P,dbls=list()){
+  D <- nmz <- list()
+  nd <- 1
+  for(nm in names(P)){
+    D[[nm]] <- data.table::data.table(P[[nm]]$r(N));
+    if(ncol(D[[nm]])==2){
+      if(nd<=length(dbls)){
+        nmz[[nm]] <- colnames(D[[nm]]) <- dbls[[nd]]
+        nd <- nd+1
+      }
+    } else {
+      names(D[[nm]]) <- nm
+      nmz[[nm]] <- nm
+    }
+  }
+  D <- data.table::setDT(unname(unlist(D, recursive = FALSE)))
+  nmz <- unname(unlist(nmz))
+  names(D) <- nmz
+  return(D)
+}
+
+## TODO could extend this to LHS sampling
