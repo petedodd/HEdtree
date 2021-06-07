68eda61308df26759ae24f00f0319fa6f96351c3
petedodd <p.j.dodd@sheffield.ac.uk>
2021-06-07 09:26:24 +0100

    tweaking makePSA to accept non-random parameters

--- R/makePSA.R
+++ R/makePSA.R
@@ -9,24 +9,28 @@
 ##' @import data.table
 ##' @export
 makePSA <- function(N,P,dbls=list()){
-  D <- nmz <- list()
-  nd <- 1
-  for(nm in names(P)){
-    D[[nm]] <- data.table::data.table(P[[nm]]$r(N));
-    if(ncol(D[[nm]])==2){
-      if(nd<=length(dbls)){
-        nmz[[nm]] <- colnames(D[[nm]]) <- dbls[[nd]]
-        nd <- nd+1
-      }
-    } else {
-      names(D[[nm]]) <- nm
-      nmz[[nm]] <- nm
+    D <- nmz <- list()
+    nd <- 1
+    for (nm in names(P)) {
+        if(length(P[[nm]])>1){
+            D[[nm]] <- data.table::data.table(P[[nm]]$r(N))
+        } else { #non-random parameters
+            D[[nm]] <- data.table::data.table(rep(P[[nm]],N))
+        }
+        if (ncol(D[[nm]]) == 2) {
+            if (nd <= length(dbls)) {
+                nmz[[nm]] <- colnames(D[[nm]]) <- dbls[[nd]]
+                nd <- nd + 1
+            }
+        }
+        else {
+            names(D[[nm]]) <- nm
+            nmz[[nm]] <- nm
+        }
     }
-  }
-  D <- data.table::setDT(unname(unlist(D, recursive = FALSE)))
-  nmz <- unname(unlist(nmz))
-  names(D) <- nmz
-  return(D)
+    D <- data.table::setDT(unname(unlist(D, recursive = FALSE)))
+    nmz <- unname(unlist(nmz))
+    names(D) <- nmz
+    return(D)
 }
-
 ## TODO could extend this to LHS sampling
